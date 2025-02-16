32b22543e292efcab1167e528a8bc98dd20961fb
grace <calianese.g@northeastern.edu>
2025-02-16 17:56:18 -0500

    highlight key

--- src/Keyboard/PianoKey.tsx
+++ src/Keyboard/PianoKey.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 
 interface Note {
@@ -9,30 +9,58 @@ interface Note {
 }
 
 export default function PianoKey({ note }: { note: Note }) {
+    const [isPressed, setIsPressed] = useState(false);
+
     const handleClick = () => {
         const audio = new Audio(note.file);
         audio.play();
     };
 
-    const handleKeyPress = (event: KeyboardEvent) => {
+    const handleMouseDown = () => {
+        setIsPressed(true);
+    };
+
+    const handleMouseUp = () => {
+        setIsPressed(false);
+    };
+
+    const handleKeyDown = (event: KeyboardEvent) => {
         if (event.key.toUpperCase() === note.key.toUpperCase()) {
-            handleClick();
+            setIsPressed(true);
+            const audio = new Audio(note.file);
+            audio.play();
+        }
+    };
+
+    const handleKeyUp = (event: KeyboardEvent) => {
+        if (event.key.toUpperCase() === note.key.toUpperCase()) {
+            setIsPressed(false);
         }
     };
 
     useEffect(() => {
-        window.addEventListener("keydown", handleKeyPress);
+        window.addEventListener("keydown", handleKeyDown);
+        window.addEventListener("keyup", handleKeyUp);
+
         return () => {
-            window.removeEventListener("keydown", handleKeyPress);
+            window.removeEventListener("keydown", handleKeyDown);
+            window.removeEventListener("keyup", handleKeyUp);
         };
     }, [note.key]);
 
+
+    const buttonClass = `${note.name.includes("#") ? "black-key" : "white-key"} ${isPressed ? "active-key" : ""
+        }`;
+
     return (
         <Button
-            className={note.name.includes("#") ? "black-key" : "white-key"}
+            className={buttonClass}
             id={`p-${note.name}`}
             onClick={handleClick}
+            onMouseDown={handleMouseDown}
+            onMouseUp={handleMouseUp}
         >
             <label className="key-label">{note.name}</label>
-        </Button>);
+        </Button>
+    );
 }
