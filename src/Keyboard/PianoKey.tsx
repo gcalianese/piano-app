ee0e9e8226294444a3ddd7e5b4b05323358ee7c8
grace <calianese.g@northeastern.edu>
2025-02-16 17:39:01 -0500

    on key press

--- src/Keyboard/PianoKey.tsx
+++ src/Keyboard/PianoKey.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "react-bootstrap";
 
 interface Note {
     name: string;
     file: string;
     color: string;
+    key: string;
 }
 
 export default function PianoKey({ note }: { note: Note }) {
@@ -12,6 +13,20 @@ export default function PianoKey({ note }: { note: Note }) {
         const audio = new Audio(note.file);
         audio.play();
     };
+
+    const handleKeyPress = (event: KeyboardEvent) => {
+        if (event.key.toUpperCase() === note.key.toUpperCase()) {
+            handleClick();
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("keydown", handleKeyPress);
+        return () => {
+            window.removeEventListener("keydown", handleKeyPress);
+        };
+    }, [note.key]);
+
     return (
         <Button
             className={note.name.includes("#") ? "black-key" : "white-key"}
